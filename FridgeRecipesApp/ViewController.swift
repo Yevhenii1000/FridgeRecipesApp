13f9b229692e6b864a60033a83c464f28813a8b1
Yevhenii <yevhenii@macbook-pro-yevhenii.local>
2017-11-23 00:00:59 +0200

    Initial Commit

---
+++ FridgeRecipesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FridgeRecipesApp
+//
+//  Created by Yevhenii on 23.11.2017.
+//  Copyright © 2017 Yevhenii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
